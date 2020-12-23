b4b65ad55cdec68650d5d740221bfb86fa7e9735
Khatya Khakhutaishvili <kkhatya@gmail.com>
2020-12-23 23:41:52 +0300

    feature/404

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 const usersRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
+const pageNotFound = require('./routes/pageNotFound');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRoutes);
 app.use('/', cardsRoutes);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', pageNotFound);
 app.listen(PORT, () => {
 });
