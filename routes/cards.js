0e5144bf45029f1384038ab0cb48472de11d09a1
Khatya Khakhutaishvili <kkhatya@gmail.com>
2020-12-24 15:57:07 +0300

    fix/review

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const filepath = path.join(__dirname, '..', 'data', 'cards.json');
-const cards = fs.readFileSync(filepath, 'utf8');
 
-router.get('/cards', (req, res) => {
-  res.send(cards);
-});
+router.get('/cards', (req, res) => fs.readFile(filepath, 'utf8')
+  .then((cards) => res.status(200).send(JSON.parse(cards)))
+  .catch((err) => res.status(500).send({ message: err.message })));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,27 @@
 const router = require('express').Router();
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const filepath = path.join(__dirname, '..', 'data', 'users.json');
-const users = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+// const users = JSON.parse(fs.readFile(filepath, 'utf8'));
 
-router.get('/users', (req, res) => {
-  res.send(users);
-});
+router.get('/users', (req, res) => fs.readFile(filepath, 'utf8')
+  .then((users) => res.status(200).send(JSON.parse(users)))
+  .catch((err) => res.status(500).send({ message: err.message })));
 
 router.get('/users/:id', (req, res) => {
   const { id } = req.params;
-  const userInfo = users.find((item) => item._id === id);
+  return fs.readFile(filepath, 'utf8')
+    .then((users) => {
+      const userInfo = JSON.parse(users).find((item) => item._id === id);
 
-  if (!userInfo) {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-    return;
-  }
-
-  res.send(userInfo);
+      if (!userInfo) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.status(200).send(userInfo);
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
 });
 
 module.exports = router;
