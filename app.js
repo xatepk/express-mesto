ac119eb84062551a0877ab000145932dd3da0a8a
Khatya Khakhutaishvili <kkhatya@gmail.com>
2021-01-21 12:54:39 +0300

    feature:mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const usersRoutes = require('./routes/users');
 const cardsRoutes = require('./routes/cards');
 const pageNotFound = require('./routes/pageNotFound');
@@ -7,6 +8,12 @@ const pageNotFound = require('./routes/pageNotFound');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRoutes);
